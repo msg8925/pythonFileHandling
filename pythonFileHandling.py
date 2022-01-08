59072fa989ea4a32f8c16ca74cdb5babed5219ba
unknownAuthor <msg.8925@gmail.com>
2022-01-07 23:07:32 -0500

    Read from a file called test.txt

--- pythonFileHandling.py
+++ pythonFileHandling.py
@@ -1,13 +1,19 @@
 #! /bin/python3 
 
-# with open('test.txt', 'r') as f:
+with open('test.txt', 'r') as f:
+    f_contents = f.read()
+    print(f_contents)
 
-f = open('test.txt', 'r+') #open a file for reading and writing
+    
 
-print(f.name)
-print(f.mode)
 
-f.close()
+
+#f = open('test.txt', 'r+') #open a file for reading and writing
+
+#print(f.name)
+#print(f.mode)
+
+#f.close()
 
 
 

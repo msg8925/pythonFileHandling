f26d133e89a11059dbd995ab79eb08cdf0efab28
unknownAuthor <msg.8925@gmail.com>
2022-01-08 02:54:32 -0500

    Use of the split function to put data from txt file into a list.

--- pythonFileHandling.py
+++ pythonFileHandling.py
@@ -1,13 +1,32 @@
 #! /bin/python3 
 
-with open('test.txt', 'r') as f:
-    f_contents = f.readline()
-    print(f_contents)
+with open('test.txt', 'r+') as f:
+   # f_contents = f.readline()
+   # print(f_contents)
 
-    f_contents = f.readline()
-    print(f_contents)
+    listOfPasswds = f.read().split()
+    print(listOfPasswds)
+    print("\n")
+    print(listOfPasswds[0])
+    print(listOfPasswds[5])
 
+    #for password in f:
+        #print(password)
+        #f_contents = f.readline()
+        #print(f_contents)
+        
 
+    #f_contents = f.readlines()
+    #print(f_contents)
+
+    #f.seek(0, 2) #seek to end of file
+
+    #f.write("frog\n")
+
+    #f.seek(0, 0) #seek to start of file
+
+    #f_contents = f.readlines()
+    #:print(f_contents)
 
 #f = open('test.txt', 'r+') #open a file for reading and writing
 
